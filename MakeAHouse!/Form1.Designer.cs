05dca3edd1ca427b851bbc04258d41159f7a899f
Zarkii <35203783+Zarkii@users.noreply.github.com>
2018-08-27 22:52:09 +0200

    Zaczeto prace nad SI!

--- MakeAHouse!/Form1.Designer.cs
+++ MakeAHouse!/Form1.Designer.cs
@@ -28,20 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Description = new System.Windows.Forms.TextBox();
+            this.GoHere = new System.Windows.Forms.Button();
+            this.Exits = new System.Windows.Forms.ComboBox();
+            this.GoThroughTheDoor = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // Description
+            // 
+            this.Description.Location = new System.Drawing.Point(12, 12);
+            this.Description.Multiline = true;
+            this.Description.Name = "Description";
+            this.Description.Size = new System.Drawing.Size(349, 259);
+            this.Description.TabIndex = 0;
+            // 
+            // GoHere
+            // 
+            this.GoHere.Location = new System.Drawing.Point(12, 277);
+            this.GoHere.Name = "GoHere";
+            this.GoHere.Size = new System.Drawing.Size(75, 23);
+            this.GoHere.TabIndex = 1;
+            this.GoHere.Text = "Idz tutaj";
+            this.GoHere.UseVisualStyleBackColor = true;
+            this.GoHere.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Exits
+            // 
+            this.Exits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Exits.FormattingEnabled = true;
+            this.Exits.Location = new System.Drawing.Point(94, 277);
+            this.Exits.Name = "Exits";
+            this.Exits.Size = new System.Drawing.Size(267, 21);
+            this.Exits.TabIndex = 2;
+            // 
+            // GoThroughTheDoor
+            // 
+            this.GoThroughTheDoor.Location = new System.Drawing.Point(12, 306);
+            this.GoThroughTheDoor.Name = "GoThroughTheDoor";
+            this.GoThroughTheDoor.Size = new System.Drawing.Size(349, 23);
+            this.GoThroughTheDoor.TabIndex = 3;
+            this.GoThroughTheDoor.Text = "Idz przez drzwi";
+            this.GoThroughTheDoor.UseVisualStyleBackColor = true;
+            this.GoThroughTheDoor.Click += new System.EventHandler(this.GoThroughTheDoor_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(519, 482);
+            this.ClientSize = new System.Drawing.Size(373, 341);
+            this.Controls.Add(this.GoThroughTheDoor);
+            this.Controls.Add(this.Exits);
+            this.Controls.Add(this.GoHere);
+            this.Controls.Add(this.Description);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Zbadaj Dom";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox Description;
+        private System.Windows.Forms.Button GoHere;
+        private System.Windows.Forms.ComboBox Exits;
+        private System.Windows.Forms.Button GoThroughTheDoor;
     }
 }
 
--- MakeAHouse!/Form1.cs
+++ MakeAHouse!/Form1.cs
@@ -12,15 +12,83 @@ namespace MakeAHouse_
 {
     public partial class Form1 : Form
     {
+        
+
+
+
+
+
+
         public Form1()
         {
             InitializeComponent();
             CreateObjects();
+            
+            
+
+
+
+
+
+
         }
 
         public void CreateObjects()
         {
+            RoomWithDoor livingRoom = new RoomWithDoor("salon", "antyczny dywan", "dębowe drzwi z mosiężną klamką");
+            Room EatingRoom = new Room("jadalnia", "steampunkowy stól");
+            RoomWithDoor Kitchen = new RoomWithDoor("kuchnia", "elektroniczna kuchenka", "szklane drzwi");
+            OutsideWithDoor InFrontOfHouse = new OutsideWithDoor("podwórko przed domem", false);
+            Outside garden = new Outside("ogrod", false);
+            OutsideWithDoor BehindHouse = new OutsideWithDoor("za domem", true);
 
+            InFrontOfHouse.Exits = new Location[]{BehindHouse,garden};
+            garden.Exits = new Location[]{InFrontOfHouse,BehindHouse};
+            BehindHouse.Exits = new Location[]{garden,InFrontOfHouse};
+            livingRoom.Exits = new Location[]{InFrontOfHouse,EatingRoom};
+            EatingRoom.Exits = new Location[]{Kitchen,livingRoom};
+            Kitchen.Exits = new Location[]{BehindHouse,EatingRoom};
+
+            CurrentLocation = livingRoom;
+
+
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MoveToANewLocation(CurrentLocation.Exits[Exits.SelectedIndex]);
+        }
+
+        void MoveToANewLocation(Location newLocation)
+        {
+            CurrentLocation = newLocation;
+            Exits.Items.Clear();
+            for (int i = 0; i < CurrentLocation.Exits.Length ; i++)
+            {
+                Exits.Items.Add(CurrentLocation.Exits[i].Name);
+            }
+
+            Exits.SelectedIndex = 0;
+            Description.Text = CurrentLocation.Description;
+            if (CurrentLocation is IHasExteriorDoor)
+            {
+                GoThroughTheDoor.Visible = true;
+            }
+            else
+            {
+                GoThroughTheDoor.Visible = false;
+
+            }
+        }
+        Location CurrentLocation;
+
+        private void GoThroughTheDoor_Click(object sender, EventArgs e)
+        {
+            IHasExteriorDoor locationtogo = CurrentLocation as IHasExteriorDoor;
+            MoveToANewLocation(locationtogo.DoorLocation);
         }
     }
 }
